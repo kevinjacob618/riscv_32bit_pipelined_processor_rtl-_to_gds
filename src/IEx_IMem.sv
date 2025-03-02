5250bef3d32301bbaa6a70d42f57412c4778f35e
kevinjacob618 <142043462+kevinjacob618@users.noreply.github.com>
2025-03-02 18:12:32 +0530

    IEx_IMem.sv

---
+++ src/IEx_IMem.sv
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+
+
+
+module IEx_IMem(input logic clk, reset,
+                input logic [31:0] ALUResultE, WriteDataE, 
+                input logic [4:0] RdE, 
+                input logic [31:0] PCPlus4E,
+                output logic [31:0] ALUResultM, WriteDataM,
+                output logic [4:0] RdM, 
+                output logic [31:0] PCPlus4M);
+
+always_ff @( posedge clk ) begin 
+    if (reset) begin
+        ALUResultM <= 0;
+        WriteDataM <= 0;
+        RdM <= 0; 
+        PCPlus4M <= 0;
+    end
+
+    else begin
+        ALUResultM <= ALUResultE;
+        WriteDataM <= WriteDataE;
+        RdM <= RdE; 
+        PCPlus4M <= PCPlus4E;        
+    end
+    
+end
+
+endmodule
